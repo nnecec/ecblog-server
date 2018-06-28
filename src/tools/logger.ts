c4167eba3881d4519fc5a1626b0023460b954ec4
nnecec <nnecec@outlook.com>
2018-06-28 19:18:19 +0800

    update

--- src/tools/logger.ts
+++ src/tools/logger.ts
@@ -1,18 +1,10 @@
 import * as winston from 'winston'
-import * as convert from 'koa-convert'
 
-import config from '../config'
-import { forEach, isFunction } from 'lodash'
+const logger = winston.createLogger({
+  level: 'debug',
+  transports: [
+    new winston.transports.Console()
+  ]
+})
 
-function buildLoggerConfig (config) {
-  const loggerConfig = Object.assign({}, config)
-  loggerConfig.transports = []
-  forEach(config.transports, (transport, key) => {
-    if (isFunction(winston.transports[key])) {
-      loggerConfig.transports.push(new winston.transports[key](transport))
-    }
-  })
-  return loggerConfig
-}
-
-export default new winston.Logger(buildLoggerConfig(config.logger))
+export default logger
